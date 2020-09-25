d59598539b6924eb00fd5a36f275093fc9187df2
Ethan Grantz <e284g507@ku.edu>
2020-09-25 11:16:32 -0500

    Created AI component

--- src/components.js
+++ src/components.js
@@ -2,6 +2,7 @@ import GameBoardComponent from './components/GameBoard.component.js'
 import GridCellComponent from './components/GridCell.component.js'
 import TopLevelComponent from './components/TopLevel.component.js'
 import ScoreBoardComponent from './components/ScoreBoard.component.js'
+import AI from './components/AI.component.js'
 
 /*
  * This is where various components we define should be registered.
@@ -12,4 +13,5 @@ export default {
     GridCellComponent,
     TopLevelComponent,
     ScoreBoardComponent,
+    AI,
 }
---
+++ src/components/AI.component.js
@@ -0,0 +1,11 @@
+import {Component} from '../../lib/vues6.js'
+
+const template = `<div></div>`
+
+class AI extends Component {
+	static get selector() { return 'ai' }
+	static get template() { return template }
+	static get props() { return [] }
+}
+
+export default AI
